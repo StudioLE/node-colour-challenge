d8667b809c2908fc1ee5ee77f8c49ab59e645e49
Laurence Elsdon <git@elsdon.me>
2015-01-01 23:28:54 +0000

    Added routes to server to enable style.css

--- server.js
+++ server.js
@@ -9,19 +9,46 @@ mst = require('mustache')
 config = require('./config')
 util = require('./lib/util')
 
-// Read the current JSON log file
-var log = util.readLog(config.log_json_file)
-
-// Read the template
-var template = fs.readFileSync(config.view_file)
-
-// Render the template with colours from log
-var output = mst.render(template.toString(), { colours: log });
-
 // Create a server
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(output);
+
+	// Status unless stated otherwise
+	var status = 200
+
+	// Content type unless otherwise stated
+	var content_type = 'text/html'
+
+	if(req.url == '/') {
+		// Read the current JSON log file
+		var log = util.readLog(config.log_json_file)
+
+		// Read the template
+		var template = fs.readFileSync(config.view_file)
+
+		// Render the template with colours from log
+		var output = mst.render(template.toString(), { colours: log })
+	}
+	else if(req.url ==  '/style.css') {
+		// Read the css file
+		var output = fs.readFileSync(config.style_file)
+
+		// Set content_type
+		var content_type = 'text/css'
+	}
+	else {
+		// Read the css file
+		var output = '404 Page Not Found'
+
+		// Change status
+		var status = 404
+	}
+
+	// Write the headers
+	res.writeHead(status, {'Content-Type': content_type})
+
+	// Send the result with output
+	res.end(output)
+	
 }).listen(config.server_port, config.server_address);
 
 console.log('Server running at http://%s:%s/', config.server_address, config.server_port);
